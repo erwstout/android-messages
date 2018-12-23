7ad4165057547faabd1f265b892327c8e571a165
Eric Stout <ericwstout@gmail.com>
2018-12-22 21:34:00 -0700

    :sparkles:

---
+++ main.js
@@ -0,0 +1,34 @@
+const { app, BrowserWindow } = require("electron");
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    title: "Android Messages",
+    darkTheme: true
+  });
+  mainWindow.loadURL("https://messages.android.com");
+  mainWindow.setTitle("Android Messages");
+
+  // Emitted when the window is closed.
+  mainWindow.on("closed", function() {
+    mainWindow = null;
+  });
+}
+app.on("ready", createWindow);
+
+// Quit when all windows are closed.
+app.on("window-all-closed", function() {
+  // On macOS it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", function() {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
